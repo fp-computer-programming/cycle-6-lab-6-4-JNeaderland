c09405ce812008eb52b08d96e680e8a92778b4d0
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2023-11-08 13:47:56 -0500

    Add files via upload

---
+++ Lab 6-4.py
@@ -0,0 +1,21 @@
+"""
+Create a Python file named lab_6-4
+
+*** You must write a comment for every chunk of code you write from now on along with your author tag***
+
+Create a list of 3 subjects that you have studied at Prep and store it as a variable.
+Use a method to add a fourth subject you have studied to the end of the list.
+Use a method to return the index of one of the subjects in your list.
+Order the subjects alphabetically using a method.
+Use a method to make a copy of this list and store it in a different variable.
+Use a method to order this second list in reverse alphabetical order.
+"""
+#Author: Jacob Neaderland
+classes = ["Calc AB", "Calc BC", "Coding"]
+classes.append("Film")
+print (classes.index("Calc BC"))
+classes.sort()
+print (classes)
+classop = classes[:]
+classop.reverse()
+print (classop)
